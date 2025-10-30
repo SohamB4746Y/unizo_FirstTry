9d1718213f236c5fe226f7eec7d6085936a2921e
SohamB4746Y <sohambhattacharya023@gmail.com>
2025-10-30 09:30:29 +0530

    Initial Commit

---
+++ unizo_FirstTryTests/unizo_FirstTryTests.swift
@@ -0,0 +1,17 @@
+//
+//  unizo_FirstTryTests.swift
+//  unizo_FirstTryTests
+//
+//  Created by Soham Bhattacharya on 30/10/25.
+//
+
+import Testing
+@testable import unizo_FirstTry
+
+struct unizo_FirstTryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
